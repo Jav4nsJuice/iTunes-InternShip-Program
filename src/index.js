81a9142bfd18d5fa2dcdfd163331b12ef654674c
Jav4nsJuice <javans.ferrel@gmail.com>
2023-02-03 16:18:32 -0400

    Search field and search button added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
+// import ItemsList from './ItemsList';
+// import { itunesApiRequest, mediaTypes } from './utils';
 import styled, { createGlobalStyle } from 'styled-components';
 import Palette from './palette';
 
@@ -8,27 +10,41 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     height: 100%;
     width: 100%;
-		margin: 0;
-		background-color: ${Palette('Grey', 800)};
+        margin: 0;
+        background-color: ${Palette('Grey', 300)};
   }
 `;
 
 const Content = styled.div`
-	width: 100%;
-	height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 class App extends React.Component {
-	render() {
-		return (
-			<>
-				<GlobalStyle />
-				<Content>
-					<Header />
-				</Content>
-			</>
-		);
-	}
+    // constructor(props) {
+        // super(props);
+        // this.state = { searchResults: [] };
+        // this.updateSearch = this.updateSearch.bind(this);
+    // }
+
+    // async updateSearch(text, media) {
+    //  const response = await itunesApiRequest(text, 'music', 'album');
+    //  this.setState({ searchResults: response.results });
+    // }
+
+    render() {
+        // const { searchResults } = this.state;
+        return (
+            <>
+                <GlobalStyle />
+                <Content>
+                    {/* <Header mediaTypes={mediaTypes} startSearch={this.updateSearch} /> */}
+                    <Header />
+                    {/* <ItemsList items={searchResults} /> */}
+                </Content>
+            </>
+        );
+    }
 }
 
 const rootElement = document.getElementById('root');
